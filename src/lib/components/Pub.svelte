ee41e546ff47ac380da945198693f3f5b92aa0a4
DandyJoshua14 <dandyjoshua14@gmail.com>
2025-04-29 18:16:29 +0100

    fix: signup env variable

---
+++ src/lib/components/Pub.svelte
@@ -0,0 +1,353 @@
+<script>
+	let docs = {
+		publications: [
+			{
+				fileUrl:
+					'https://unctad.org/publication/empretec-women-business-building-purpose-driven-enterprises-amid-crises',
+				title: 'Empretec Women in Business: Building purpose-driven enterprises amid crises',
+				code: 'UNCTAD/DIAE/ED/2022/1',
+				datePublished: '21 Nov 2022',
+				imageUrl: 'https://yrka.org/gir37'
+			},
+			{
+				fileUrl: 'https://unctad.org/publication/empretec-women-business-awards-2020',
+				title: 'Empretec Women in Business Awards 2020',
+				code: 'UNCTAD/DIAE/ED/2020/1',
+				datePublished: '2 Nov 2020',
+				imageUrl: 'https://yrka.org/en8kq'
+			},
+			{
+				fileUrl: 'https://unctad.org/publication/empretec-women-business-awards-2018',
+				title: 'Empretec Women in Business Awards 2018',
+				code: 'UNCTAD/DIAE/ED/2018/1',
+				datePublished: '15 Oct 2018',
+				imageUrl: 'https://yrka.org/yrxzb'
+			},
+			{
+				fileUrl: 'https://unctad.org/publication/policy-guide-youth-entrepreneurship',
+				title: 'Policy Guide on Youth Entrepreneurship',
+				code: 'UNCTAD/DIAE/ED/2015/1',
+				datePublished: '24 Nov 2015',
+				imageUrl: 'https://yrka.org/6zxtb'
+			},
+			{
+				fileUrl: 'https://unctad.org/publication/empretec-women-business-awards-2014',
+				title: 'Empretec Women in Business Awards 2014',
+				code: 'UNCTAD/DIAE/ED/2014/3',
+				datePublished: '1 Dec 2014',
+				imageUrl: 'https://yrka.org/d4vn3'
+			},
+			{
+				fileUrl: 'https://unctad.org/publication/empretec-women-business-awards-2012',
+				title: 'Empretec Women in Business Awards 2012',
+				code: 'UNCTAD/DIAE/ED/2012/5',
+				datePublished: '1 Dec 2012',
+				imageUrl: ''
+			}
+		],
+		documents: [
+			{
+				type: 'Publication',
+				fileUrl: 'https://unctad.org/system/files/official-document/diaeed2022d1_en.pdf',
+				title: 'Empretec Women in Business: Building purpose-driven enterprises amid crises',
+				code: 'UNCTAD/DIAE/ED/2022/1',
+				datePublished: '21 Nov 2022',
+				imageUrl: 'https://yrka.org/9u9gz',
+				language: 'English'
+			},
+			{
+				type: 'Publication',
+				fileUrl: 'https://unctad.org/system/files/official-document/diaeedinf2022d1_en.pdf',
+				title: 'Empretec Newsletter (Issue 41)',
+				code: 'UNCTAD/DIAE/ED/INF/2022/1',
+				datePublished: '21 Feb 2022',
+				imageUrl: 'https://yrka.org/k0wpq',
+				language: 'English'
+			},
+			{
+				type: 'Publication',
+				fileUrl: 'https://unctad.org/system/files/official-document/diaeedinf2021d3_en.pdf',
+				title: 'Empretec Newsletter (Issue 40)',
+				code: 'UNCTAD/DIAE/ED/INF/2021/3',
+				datePublished: '30 Sep 2021',
+				imageUrl: 'https://yrka.org/7zcl0',
+				language: 'English'
+			},
+			{
+				type: 'Publication',
+				fileUrl: 'https://unctad.org/system/files/official-document/diaeedinf2020d5_en.pdf',
+				title: 'Empretec Newsletter (Issue 38)',
+				code: 'UNCTAD/DIAE/ED/INF/2020/5',
+				datePublished: '1 Dec 2020',
+				imageUrl: 'https://yrka.org/whr9b',
+				language: 'English'
+			},
+			{
+				type: 'Publication',
+				fileUrl: 'https://unctad.org/system/files/official-document/diaeedinf2020d4_en.pdf',
+				title: 'Empretec Newsletter (Issue 37)',
+				code: 'UNCTAD/DIAE/ED/INF/2019/4',
+				datePublished: '1 Dec 2019',
+				imageUrl: 'https://yrka.org/w4cyu',
+				language: 'English'
+			},
+			{
+				type: 'Publication',
+				fileUrl: 'https://unctad.org/system/files/official-document/diaeedinf2020d2_en.pdf',
+				title: 'Empretec Newsletter (Issue 36)',
+				code: 'UNCTAD/DIAE/ED/INF/2019/2',
+				datePublished: '1 Sep 2019',
+				imageUrl: 'https://yrka.org/8dazk',
+				language: 'English'
+			},
+			{
+				type: 'Publication',
+				fileUrl: 'https://unctad.org/system/files/official-document/diaeed2017d6_en.pdf',
+				title:
+					'Promoting Entrepreneurship for Sustainable Development: A Selection Of Business Cases from the Empretec Network',
+				code: 'UNCTAD/DIAE/ED/2017/6',
+				datePublished: '21 Dec 2018',
+				imageUrl: 'https://yrka.org/n8zle',
+				language: 'English'
+			},
+			{
+				type: 'Publication',
+				fileUrl: 'https://unctad.org/system/files/official-document/webdiaeed2018d1_en.pdf',
+				title: 'Empretec Newsletter (Issue 35)',
+				code: 'UNCTAD/WEB/DIAE/ED/2018/01',
+				datePublished: '11 Dec 2018',
+				imageUrl: 'https://yrka.org/1o2sp',
+				language: 'English'
+			},
+			{
+				type: 'Publication',
+				fileUrl: 'https://unctad.org/system/files/official-document/webdiaeed2018d2_en.pdf',
+				title: 'Empretec Newsletter (Issue 34)',
+				code: 'UNCTAD/WEB/DIAE/ED/2018/02',
+				datePublished: '1 Nov 2018',
+				imageUrl: 'https://yrka.org/b2fwb',
+				language: 'English'
+			},
+			{
+				type: 'Publication',
+				fileUrl: 'https://unctad.org/system/files/official-document/webdiaeed2015d1_en.pdf',
+				title: 'Policy Guide on Youth Entrepreneurship',
+				code: 'UNCTAD/DIAE/ED/2015/1',
+				datePublished: '24 Nov 2015',
+				imageUrl: 'https://yrka.org/otz4n',
+				language: 'English'
+			},
+			{
+				type: 'Publication',
+				fileUrl: 'https://unctad.org/system/files/official-document/diaeed2014d2_en.pdf',
+				title: 'Empretec Annual Report 2013',
+				code: 'UNCTAD/DIAE/ED/2014/2',
+				datePublished: '27 Aug 2015',
+				imageUrl: 'https://yrka.org/vz22x',
+				language: 'English'
+			},
+			{
+				type: 'Publication',
+				fileUrl: 'https://unctad.org/system/files/official-document/webdiaeed2015d2_en.pdf',
+				title: 'Empretec Newsletter (Issue 28)',
+				code: 'UNCTAD/WEB/DIAE/ED/2015/2',
+				datePublished: '24 Jul 2015',
+				imageUrl: 'https://yrka.org/ylkx4',
+				language: 'English'
+			},
+			{
+				type: 'Publication',
+				fileUrl: 'https://unctad.org/official-documents-search?f[0]=sitemap%3A836',
+				title: 'Empretec Women in Business Awards 2014',
+				code: 'UNCTAD/DIAE/ED/2014/3',
+				datePublished: '1 Dec 2014',
+				imageUrl: '',
+				language: 'English'
+			},
+			{
+				type: 'Publication',
+				fileUrl: 'https://unctad.org/system/files/official-document/diaeed2012d5_en.pdf',
+				title: 'Empretec Women in Business Awards 2012',
+				code: 'UNCTAD/DIAE/ED/2012/5',
+				datePublished: '1 Dec 2012',
+				imageUrl: 'https://yrka.org/xmqy7',
+				language: 'English'
+			},
+			{
+				type: 'Publication',
+				fileUrl: 'https://unctad.org/system/files/official-document/webdiaeed2010_May_en.pdf',
+				title: 'Empretec Newsletter (Issue 12)',
+				code: 'UNCTAD/WEB/DIAE/ED/2010/1',
+				datePublished: '1 May 2010',
+				imageUrl: '',
+				language: 'English'
+			},
+			{
+				type: 'Publication',
+				fileUrl: 'https://unctad.org/system/files/official-document/webdiaeed2009_Dec_en.pdf',
+				title: 'Empretec Newsletter (Issue 11)',
+				code: 'UNCTAD/WEB/DIAE/ED/2009/4',
+				datePublished: '1 Dec 2009',
+				imageUrl: '',
+				language: 'English'
+			},
+			{
+				type: 'Publication',
+				fileUrl: 'https://unctad.org/system/files/official-document/webdiaeed2009_Oct_en.pdf',
+				title: 'Empretec Newsletter (Issue 10)',
+				code: 'UNCTAD/WEB/DIAE/ED/2009/3',
+				datePublished: '1 Sep 2009',
+				imageUrl: '',
+				language: 'English'
+			},
+			{
+				type: 'Publication',
+				fileUrl: 'https://unctad.org/system/files/official-document/webdiaeed2009_Apr_en.pdf',
+				title: 'Empretec Newsletter (Issue 8)',
+				code: 'UNCTAD/WEB/DIAE/ED/2009/1',
+				datePublished: '1 Apr 2009',
+				imageUrl: '',
+				language: 'English'
+			},
+			{
+				type: 'Publication',
+				fileUrl: 'https://unctad.org/system/files/official-document/webdiaeed2008_Jun_en.pdf',
+				title: 'Empretec Newsletter (Issue 6)',
+				code: 'UNCTAD/WEB/DIAE/ED/2008/1',
+				datePublished: '1 Jun 2008',
+				imageUrl: '',
+				language: 'English'
+			},
+			{
+				type: 'Publication',
+				fileUrl: 'https://unctad.org/system/files/official-document/webdiaeed2007_Aug_en.pdf',
+				title: 'Empretec Newsletter (Issue 3)',
+				code: 'UNCTAD/WEB/DIAE/ED/2007/1',
+				datePublished: '1 Aug 2007',
+				imageUrl: '',
+				language: 'English'
+			}
+		]
+	};
+</script>
+
+<!-- <h3>Documents</h3>
+<div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 p-4">
+	{#each docs.documents as d}
+		<div
+			class="bg-white rounded-lg shadow-md overflow-hidden flex flex-col w-full max-w-sm mx-auto"
+		>
+			<img src={d.imageUrl} alt="Publication Cover" class="h-48 w-full object-cover" />
+
+			<div class="p-4 flex flex-col space-y-2">
+				<span class="text-xs uppercase text-gray-500">{d.type || ''}</span>
+
+				<h3 class="text-lg font-semibold text-gray-900 leading-snug">{d.title}</h3>
+				<p class="text-sm text-gray-700">{d.code}</p>
+
+				<p class="text-sm text-gray-500">{d.datePublished}</p>
+
+				<a
+					href={d.fileUrl}
+					target="_blank"
+					class="text-blue-600 hover:underline text-sm font-medium"
+				>
+					Read →
+				</a>
+			</div>
+		</div>
+	{/each}
+</div>
+
+<div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 p-4">
+	{#each docs.publications as d}
+		<div
+			class="bg-white rounded-lg shadow-md overflow-hidden flex flex-col w-full max-w-sm mx-auto"
+		>
+			<img src={d.imageUrl} alt="Publication Cover" class="h-48 w-full object-cover" />
+
+			<div class="p-4 flex flex-col space-y-2">
+				<h3 class="text-lg font-semibold text-gray-900 leading-snug">{d.title}</h3>
+				<p class="text-sm text-gray-700">{d.code}</p>
+
+				<p class="text-sm text-gray-500">{d.datePublished}</p>
+
+				<a
+					href={d.fileUrl}
+					target="_blank"
+					class="text-blue-600 hover:underline text-sm font-medium"
+				>
+					Read →
+				</a>
+			</div>
+		</div>
+	{/each}
+</div> -->
+
+<h3 class="font-bold px-3">Documents</h3>
+<div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 p-4">
+	{#each docs.documents as d}
+		<div
+			class="bg-white rounded-lg shadow-md overflow-hidden flex flex-col w-full max-w-sm mx-auto"
+		>
+			{#if d.imageUrl}
+				<img src={d.imageUrl} alt="Publication Cover" class="h-48 w-full object-cover" />
+			{:else}
+				<div
+					class="h-48 w-full"
+					style="background: linear-gradient(to bottom right, #00adef, #f7701f);"
+				></div>
+			{/if}
+
+			<div class="p-4 flex flex-col space-y-2">
+				<span class="text-xs uppercase text-gray-500">{d.type || ''}</span>
+
+				<h3 class="text-lg font-semibold text-gray-900 leading-snug">{d.title}</h3>
+				<p class="text-sm text-gray-700">{d.code}</p>
+
+				<p class="text-sm text-gray-500">{d.datePublished}</p>
+
+				<a
+					href={d.fileUrl}
+					target="_blank"
+					class="text-blue-600 hover:underline text-sm font-medium"
+				>
+					Read →
+				</a>
+			</div>
+		</div>
+	{/each}
+</div>
+
+<h3 class="font-bold px-3">Publications</h3>
+<div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 p-4">
+	{#each docs.publications as d}
+		<div
+			class="bg-white rounded-lg shadow-md overflow-hidden flex flex-col w-full max-w-sm mx-auto"
+		>
+			{#if d.imageUrl}
+				<img src={d.imageUrl} alt="Publication Cover" class="h-48 w-full object-cover" />
+			{:else}
+				<div
+					class="h-48 w-full"
+					style="background: linear-gradient(to bottom right, #00adef, #f7701f);"
+				></div>
+			{/if}
+
+			<div class="p-4 flex flex-col space-y-2">
+				<h3 class="text-lg font-semibold text-gray-900 leading-snug">{d.title}</h3>
+				<p class="text-sm text-gray-700">{d.code}</p>
+
+				<p class="text-sm text-gray-500">{d.datePublished}</p>
+
+				<a
+					href={d.fileUrl}
+					target="_blank"
+					class="text-blue-600 hover:underline text-sm font-medium"
+				>
+					Read →
+				</a>
+			</div>
+		</div>
+	{/each}
+</div>
--- src/routes/(auth)/testfunctions/+page.svelte
+++ src/routes/(auth)/testfunctions/+page.svelte
@@ -1,6 +1,52 @@
 <script lang="ts">
+	import Pub from '$lib/components/Pub.svelte';
 	import { addToCart, refreshCart } from '$lib/controls.svelte';
 	import { Button } from 'carbon-components-svelte';
+	let publications = [
+		{
+			fileUrl:
+				'https://unctad.org/publication/empretec-women-business-building-purpose-driven-enterprises-amid-crises',
+			title: 'Empretec Women in Business: Building purpose-driven enterprises amid crises',
+			code: 'UNCTAD/DIAE/ED/2022/1',
+			datePublished: '21 Nov 2022',
+			imageUrl: 'https://yrka.org/gir37'
+		},
+		{
+			fileUrl: 'https://unctad.org/publication/empretec-women-business-awards-2020',
+			title: 'Empretec Women in Business Awards 2020',
+			code: 'UNCTAD/DIAE/ED/2020/1',
+			datePublished: '2 Nov 2020',
+			imageUrl: 'https://yrka.org/en8kq'
+		},
+		{
+			fileUrl: 'https://unctad.org/publication/empretec-women-business-awards-2018',
+			title: 'Empretec Women in Business Awards 2018',
+			code: 'UNCTAD/DIAE/ED/2018/1',
+			datePublished: '15 Oct 2018',
+			imageUrl: 'https://yrka.org/yrxzb'
+		},
+		{
+			fileUrl: 'https://unctad.org/publication/policy-guide-youth-entrepreneurship',
+			title: 'Policy Guide on Youth Entrepreneurship',
+			code: 'UNCTAD/DIAE/ED/2015/1',
+			datePublished: '24 Nov 2015',
+			imageUrl: 'https://yrka.org/6zxtb'
+		},
+		{
+			fileUrl: 'https://unctad.org/publication/empretec-women-business-awards-2014',
+			title: 'Empretec Women in Business Awards 2014',
+			code: 'UNCTAD/DIAE/ED/2014/3',
+			datePublished: '1 Dec 2014',
+			imageUrl: 'https://yrka.org/d4vn3'
+		},
+		{
+			fileUrl: 'https://unctad.org/publication/empretec-women-business-awards-2012',
+			title: 'Empretec Women in Business Awards 2012',
+			code: 'UNCTAD/DIAE/ED/2012/5',
+			datePublished: '1 Dec 2012',
+			imageUrl: ''
+		}
+	];
 </script>
 
 <Button
@@ -10,3 +56,7 @@
 	}}
 	>Add to cart !
 </Button>
+
+<div>
+	<Pub />
+</div>
